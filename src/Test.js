9f2f48cb3aa17afbe954221bf5f878165fe5ced3
PolinaStamenova <polina.stamenova@gmail.com>
2021-09-07 15:46:26 +0300

    Update Test component

--- src/Test.js
+++ src/Test.js
@@ -3,23 +3,25 @@
 import axios from "axios";
 
 const Test = () => {
-  const url = "https://api.openweathermap.org/data/2.5/find?";
+  const url = "https://api.openweathermap.org/data/2.5/weather?";
   const apiKey = "3d4b1932841f45e369d9c03b24936231";
   const city = "Berlin";
-
   const fetchApi = async () => {
     const data = await axios
       .get(`${url}q=${city}&appid=${apiKey}`)
-      .then((res) => res.data.list[1].weather[0].description)
+      .then((res) => res.data)
       .catch((error) => error);
     console.log(data);
   };
   fetchApi();
+
   return (
     <div>
-      <div>Jessi</div>
+      <div>Jessie</div>
     </div>
   );
 };
 
 export default Test;
+
+// get(`${url}q=${city}&appid=${apiKey}`)
